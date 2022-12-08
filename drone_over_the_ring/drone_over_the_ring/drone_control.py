80e73dfa523169931f072c744d3e0d59ee088eb8
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-12-08 13:45:56 +0100

    Delete image_processing as it will be moved to detection. Add pyyaml to requirements. Add coef config for drone navigation. Complete drone_control with drone_state and gate navigation phase. Fixed video stream receiver worker with delay. Working on navigation algorithm

--- drone_over_the_ring/drone_over_the_ring/drone_control.py
+++ drone_over_the_ring/drone_over_the_ring/drone_control.py
@@ -2,23 +2,50 @@ import time
 import numpy as np
 import multiprocessing as mp
 import socket
+from dataclasses import dataclass
+from enum import Enum
 import cv2.cv2 as cv
 from multiprocessing.sharedctypes import Synchronized
-from typing import Callable, List
-from gate_descriptor import GateDescriptor
+from typing import Callable
+from gate_descriptor import GateDescriptor, GateType
 from datetime import datetime
+import yaml
+
+
+class NavigationStep(Enum):
+    NOT_DETECTED = 1
+    DETECTED = 2
+    GO_THROUGH = 3
+
+
+@dataclass(init=True, repr=True, eq=True, order=False)
+class DroneState():
+    prev_dx: float = 0.0
+    dx: float = 0.0
+    prev_dy: float = 0.0
+    dy: float = 0.0
+    prev_dz: float = 0.0
+    dz: float = 0.0
+    prev_dyaw: float = 0.0
+    dyaw: float = 0.0
+    not_detected_count: int = 20
+    not_detected_limit: int = 20
+    gate_navigation_step: NavigationStep \
+        = NavigationStep.NOT_DETECTED
+    going_through_gate: bool = False
 
 
 class Drone():
 
     def __init__(self,
                  img_process_routine: Callable[[np.ndarray], GateDescriptor],
+                 navigation_config: str = "config/default_nav_config.yaml",
                  use_order: bool = True,
                  use_video: bool = True,
                  use_navigation: bool = True,
                  debug: bool = False) -> None:
 
-        self.orientation = mp.Value('i', 0)
+        self.tilt = mp.Value('i', 0)
 
         self.__use_order = use_order
         self.__use_video = use_video
@@ -34,6 +61,7 @@ class Drone():
         self.RETRY = 3
         self.TIMEOUT = 5
         self.WAITING = 2
+        self.VIDEO_STREAM_DELAY = 0.05
         self.DELAY = 0.5
         self.STOP_DELAY = 20
 
@@ -41,6 +69,23 @@ class Drone():
         self.__img_process_queue = mp.Queue()
         self.log_file = "log/drone " + str(datetime.now())
         self.log_file = self.log_file.replace(" ", "_")
+
+        self.kpx = 5.0
+        self.kdx = 4.0
+        self.krx = 20.0
+
+        self.kpy = 5.0
+        self.kdy = 4.0
+        self.krx = 20.0
+
+        self.kryaw = 10.0
+
+        with open(navigation_config, 'r') as _stream:
+            _conf = yaml.safe_load(_stream)
+            coefs = ["kpx", "kdx", "krx", "kpy", "kdy", "krx", "kryaw"]
+            for _c in coefs:
+                if _c in _conf:
+                    self.__setattr__(_c, _conf[_c])
 
         self.__order_socket.settimeout(self.TIMEOUT)
         self.__order_socket.bind(self.__server_address)
@@ -51,7 +96,7 @@ class Drone():
                 args=(self.__order_queue,
                       self.__order_socket,
                       self.log_file,
-                      self.orientation))
+                      self.tilt))
 
         self.video_receiver_worker = mp.Process(
                 target=self.__video_receiver,
@@ -64,23 +109,24 @@ class Drone():
                 args=(self.__img_process_queue,
                       self.__order_queue,
                       img_process_routine,
-                      self.orientation))
+                      self.tilt))
 
     def __order_executor(self,
                          ord_q: mp.Queue,
                          sock: socket.socket,
                          log_file: str,
-                         orientation: Synchronized) -> None:
+                         tilt: Synchronized) -> None:
 
         while True:
             _cmd = ord_q.get(block=True)
-            print(_cmd)
-            print(ord_q)
             f = open(log_file, 'a')
             f.write(_cmd + " : Waiting for response\n")
             for _ in range(self.RETRY):
                 sock.sendto(_cmd.encode('utf-8'), self.__drone_address)
                 time.sleep(self.WAITING)
+
+                # TODO: set tilt of the drone
+
                 try:
                     bytes_, _ = sock.recvfrom(1024)
                     if bytes_ == b'ok':
@@ -100,7 +146,6 @@ class Drone():
                     f.write(_cmd + " : " + str(e.args[0]) + "\n")
                     continue
             f.close()
-        # TODO: change mode depending on movement order
 
     def execute_order(self,
                       order: str) -> None:
@@ -109,6 +154,8 @@ class Drone():
     def __video_receiver(self,
                          img_q: mp.Queue,
                          debug=False) -> None:
+
+        _stream_now = time.process_time()
         while True:
             frame = b''
             while True:
@@ -118,7 +165,11 @@ class Drone():
                     break
 
             img = np.asarray(frame, dtype=np.uint8)
-            img_q.put(img)
+
+            if time.process_time() - _stream_now \
+                    >= self.VIDEO_STREAM_DELAY:
+                img_q.put(img)
+                _stream_now = time.process_time()
 
             if debug:
                 cv.imshow('frame', img)
@@ -128,16 +179,28 @@ class Drone():
     def __navigation_cycle(self,
                            img_q: mp.Queue,
                            ord_q: mp.Queue,
-                           process_routine: Callable[[np.ndarray],
+                           process_routine: Callable[[np.ndarray, float],
                                                      GateDescriptor],
-                           orientation: Synchronized) -> None:
+                           tilt: Synchronized) -> None:
 
-        img = img_q.get(block=True)
-        _gate_descriptor = process_routine(img)
-        # TODO: imgproc + choice of mode influences camera's matrix
-        # TODO: implement navigation logic
+        prev_state = (0.0, 0.0, 0.0, 0.0, False)
+        while True:
+            img = img_q.get(block=True)
+            _gate_descriptor = process_routine(img, tilt.value)
+            if _gate_descriptor.type_ == GateType.NO_GATE:
+                ord_q.put("cw 360")
+            else:
+                state = (_gate_descriptor.x,
+                         _gate_descriptor.y,
+                         _gate_descriptor.z,
+                         _gate_descriptor.pixel_width
+                         / _gate_descriptor.pixel_height,
+                         True)
+
+            # TODO: implement navigation logic
 
     def run(self) -> None:
+
         if self.__use_order:
             self.execute_order("command")
             time.sleep(self.DELAY)
@@ -170,6 +233,7 @@ class Drone():
             time.sleep(self.DELAY)
 
         self.execute_order("streamoff")
+        self.execute_order("rc 0 0 0 0")
         self.execute_order("land")
         time.sleep(self.STOP_DELAY)
         print("===== Goodbye =====")
