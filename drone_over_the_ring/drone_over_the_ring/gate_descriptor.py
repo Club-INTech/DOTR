fd7053af886fa2ea92fe6a155810a55022c4004d
gaetanbecker <gaetan.becker@telecom-sudparis.eu>
2023-01-28 22:52:57 +0100

    test detection

--- drone_over_the_ring/drone_over_the_ring/gate_descriptor.py
+++ drone_over_the_ring/drone_over_the_ring/gate_descriptor.py
@@ -37,4 +37,3 @@ class GateDescriptor():
         self.x = real_ratio * ( (x_min + x_max) / 2 - CAMERA_WIDTH/2 )
         self.z = -real_ratio * ( (y_min + y_max) / 2 - CAMERA_HEIGHT/2 ) 
         self.y = np.sqrt(self.distance**2 - self.x**2 - self.z**2)
-        
